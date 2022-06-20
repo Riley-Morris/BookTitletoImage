1a87536963e88d08ae750ab086075cb451d24faf
Riley-Morris <107701261+Riley-Morris@users.noreply.github.com>
2022-06-20 10:28:48 -0400

    Added some QoL changes
    Files will now delete after being inserted into output excel file.

--- imgexcelscr.py
+++ imgexcelscr.py
@@ -1,4 +1,4 @@
-import requests, shutil, xlsxwriter, os
+import requests, shutil, xlsxwriter, os, glob
 from bs4 import BeautifulSoup
 import pandas as pd
 #returns link to the first image in search results for book_to_find in indigo website
@@ -6,14 +6,13 @@ def generate_link(book_to_find):
     url = (f'https://www.chapters.indigo.ca/en-ca/home/search/?keywords={book_to_find}')
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
-
     imagefind = soup.find('img')
     link = imagefind['src']
     return link
 #downloads image to 'book1.jpg' in users\riley
 def download_image(link_to_image, index):
     r = requests.get(link_to_image, stream=True) #Get request on link_to_image
-    with open(f"book{index}.jpg", 'wb') as f:
+    with open(f"C:\\Users\\riley\\Documents\\Pyth\\ScrapeBin\\book{index}.jpg", 'wb') as f:
       r.raw.decode_content = True
       shutil.copyfileobj(r.raw, f)
       f.close()
@@ -23,13 +22,13 @@ def get_excel_data(fileobj, column_name):
     df = pd.DataFrame(data)
     coldata = df[column_name]
     return coldata
-
+#file to extract
 #usage example, takes 'article title' column from file below and extracts it to file1
-file1 = get_excel_data(r'C:\Users\riley\Eco Warriors kids list April 2021- LOR.xlsx', 'ARTICLE TITLE')
+file1 = get_excel_data(r'C:\Users\riley\example.xlsx', 'ARTICLE TITLE')
 
 
 # generates workbook and calls above functions to create an author's info in 1st row,
-# and an image of the cover of the book in 2nd
+# and an image of the cover of the book
 def wbgen(book_data, filename):
     workbook = xlsxwriter.Workbook(f'C:\\Users\\riley\\{filename}.xlsx')
     worksheet = workbook.add_worksheet()
@@ -38,7 +37,12 @@ def wbgen(book_data, filename):
     for i in range(1, len(book_data) +1):
         download_image(generate_link(book_data[i-1]), i)
         worksheet.write(f'A{i}', f'{book_data[i-1]}:') #writes the book data to excel file
-        worksheet.insert_image(f'B{i}', f'book{i}.jpg') #writes the corresponding images it excel file
+        worksheet.insert_image(f'B{i}', f'C:\\Users\\riley\\Documents\\Pyth\\ScrapeBin\\book{i}.jpg') #writes the corresponding images it excel file
     workbook.close()
+    #removes downloaded files from directory
+    path = r"C:\Users\riley\Documents\Pyth\ScrapeBin"
+    files = glob.glob(path + '/*.jpg')
+    for f in files:
+        os.remove(f)
 #final function call to write data from file1 into new excel file 'EcoWarriors_images'
-wbgen(file1, 'EcoWarriors_Images')
+wbgen(file1, 'Example_Output')
